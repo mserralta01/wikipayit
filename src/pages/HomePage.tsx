2817ee0ff0e51ec369bee422f79debd9ab6d2cd4
Matt Serralta <mserralta@gmail.com>
2024-11-20 06:45:20 -0500

    V1. With Admin and modification

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import HeroSection from '../components/HeroSection'
 import IndustriesSection from '../components/IndustriesSection'
 import EntrepreneurSection from '../components/EntrepreneurSection'
@@ -9,20 +9,52 @@ import PricingSection from '../components/PricingSection'
 import ACHSection from '../components/ACHSection'
 import TestimonialsSection from '../components/TestimonialsSection'
 import ContactForm from '../components/ContactForm'
+import { websiteService, type Section } from '../services/websiteService'
+
+const sectionComponents = {
+  hero: HeroSection,
+  industries: IndustriesSection,
+  entrepreneur: EntrepreneurSection,
+  pos: POSSection,
+  gateway: GatewaySection,
+  highRisk: HighRiskSection,
+  pricing: PricingSection,
+  ach: ACHSection,
+  testimonials: TestimonialsSection,
+  contact: ContactForm,
+}
 
 export default function HomePage() {
+  const [sections, setSections] = useState<Section[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const loadSections = async () => {
+      try {
+        const data = await websiteService.getSections()
+        setSections(data)
+      } catch (error) {
+        console.error('Error loading sections:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    loadSections()
+  }, [])
+
+  if (loading) {
+    return null // or a loading spinner
+  }
+
   return (
     <div className="relative">
-      <HeroSection />
-      <IndustriesSection />
-      <EntrepreneurSection />
-      <GatewaySection />
-      <POSSection />
-      <HighRiskSection />
-      <ACHSection />
-      <PricingSection />
-      <TestimonialsSection />
-      <ContactForm />
+      {sections
+        .filter((section) => section.enabled)
+        .map((section) => {
+          const Component = sectionComponents[section.id as keyof typeof sectionComponents]
+          return Component ? <Component key={section.id} /> : null
+        })}
     </div>
   )
 } 
